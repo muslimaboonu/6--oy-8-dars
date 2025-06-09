3e11a2db0bf04854fbbaa997fbf0c714750c436e
m <m>
2025-06-09 10:06:16 +0300

    asdfsaf

--- src/pages/routeController.jsx
+++ src/pages/routeController.jsx
@@ -1,26 +1,37 @@
-import {useRoutes} from "react-router-dom";
-import {lazy, Suspense} from "react";
+import { createBrowserRouter, RouterProvider, useRoutes } from "react-router-dom";
+import { lazy, Suspense } from "react";
 
-const Home = lazy(() => import("./home/home.jsx"))
-const About = lazy(() => import("./about/about.jsx"))
-const Contact = lazy(() => import("./contact/contact.jsx"))
+const Home = lazy(() => import("./home/home.jsx"));
+const About = lazy(() => import("./about/about.jsx"));
+const Contact = lazy(() => import("./contact/contact.jsx"));
 
 const RouteController = () => {
-  return (
-      useRoutes([
-        {
-          path: "",
-          element: <Suspense><Home/></Suspense>,
-        },
-        {
-          path: "about",
-          element: <Suspense><About/></Suspense>,
-        },
-        {
-          path: "contact",
-          element: <Suspense><Contact/></Suspense>,
-        }
-      ])
-  )
-}
-export default RouteController
+  const router = createBrowserRouter([
+    {
+      path: "",
+      element: (
+        <Suspense fallback={<div>Loading...</div>}>
+          <Home />
+        </Suspense>
+      ),
+    },
+    {
+      path: "about",
+      element: (
+        <Suspense fallback={<div>Loading...</div>}>
+          <About />
+        </Suspense>
+      ),
+    },
+    {
+      path: "contact",
+      element: (
+        <Suspense fallback={<div>Loading...</div>}>
+          <Contact />
+        </Suspense>
+      ),
+    },
+  ]);
+  return <RouterProvider router={router} />;
+};
+export default RouteController;
